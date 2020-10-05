c793ce2e37bcd05f600f09fb3f5b8972a7e58a4c
kiborgok <borgokalek14@gmail.com>
2020-10-05 16:23:19 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+(async () => {
+  try {
+    const app = express();
+    const port = process.env.PORT;
+
+    app.disable("x-powered-by");
+    app.use(express.json());
+    app.use(cors({ credentials: true }));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    // Serve static assets if in production
+    if (process.env.NODE_ENV === "production") {
+      // Set static folder
+      app.use(express.static("frontend/build"));
+    }
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+    });
+
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
+})();
